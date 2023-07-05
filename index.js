53277c1cdfb116e155040317a3e942ad1488db9b
shivam <shivamsaxena251@gmail.com>
2023-07-05 23:00:16 +0530

    finishing app

--- index.js
+++ index.js
@@ -1,111 +1,49 @@
-const express =require('express');
-const app= express();
-const mustacheExpress=require('mustache-express');
-const bodyParser=require('body-parser');
-const pgp= require('pg-promise')();
-const bcrypt=require('bcrypt');
-const session=require('express-session');
-const PORT=8080;
-const path=require('path');
 
-//createing partials
-const VIEWS_PATH= path.join(__dirname,'/views');
-//register session
+const express = require('express');
+const app = express();
+const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
+const pgp = require('pg-promise')();
+const session = require('express-session');
+const path = require('path');
+const checkAuthorize = require('./utils/checkAuthorization');
 
-app.use(session({
-    secret: 'firstnodeproject',
-    resave: false,
-    saveUninitialized: false
-}))
-//connect DB
+const userRoutes = require('./routes/users');
+const indexRoutes = require('./routes/app');
+
+const PORT = 8080;
 const connectString="postgres://postgres:root@localhost:5432/newsdb";   //'postgres://username:password@host:port/database';
-const db =pgp(connectString);
+const VIEWS_PATH = path.join(__dirname,'/views');
 
-//encrypt password
 
-const SALT_ROUND =10;
-
-//configure view engine
-app.engine('mustache',mustacheExpress(VIEWS_PATH+'/partials','.mustache'));
+// configuring your view engine
+app.engine('mustache',mustacheExpress(VIEWS_PATH + '/partials','.mustache'));
 app.set('views',VIEWS_PATH);
 app.set('view engine','mustache');
+
+app.use('/css',express.static('css')); // localhost:3000/css/site.css
+
+app.use(session({
+  secret: 'lhadhlsdalh',
+  resave: false,
+  saveUninitialized: false
+}));
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 
-app.get('/login',(req,res)=>{
-    res.render('login');
+app.use((req,res,next)=>{
+  res.locals.authenticated = req.session.user ==null ? false : true;
+  next();
 })
 
-app.post('/login',(req,res)=>{
-    let username=req.body.username;
-    let password=req.body.password;
-    db.oneOrNone('SELECT userid,username,password FROM users WHERE username=$1',[username])
-    .then((user)=>{
-        if(user){
-            bcrypt.compare(password,user.password,function(error,result){
-                if(result){
-                    //put username and user id in session
-                    if(req.session){
-                        req.session.user={userId: user.userid, username: user.username};
-                    }
-                    res.redirect('/users/article');
-                }
-                else{
-                    res.render('login',{ message: "invalid credentials!"})
-                }
-            })
-        }
-        else{
-            res.render('login',{ message: "user not exist!"})
-        }
-    })
-})
+db = pgp(connectString);
 
-app.get('/users/article',(req,res)=>{
-    res.render('articles',{username: req.session.user.username});
-})
-app.post('/register',(req,res)=>{
+// setup routes
+app.use('/',indexRoutes);
+app.use('/users',checkAuthorize,userRoutes);
 
-    let username=req.body.username;
-    let password=req.body.password;
 
-    db.oneOrNone('SELECT userid FROM users WHERE username = $1', [username])
-    .then((user)=>{
-        if(user) {
-            res.render('register',{ message: "user already exist!"})
-        }
-        else{
-            //insert user into the user table
-            bcrypt.hash(password,SALT_ROUND,function(error,hash){
-                if(error == null){
-                    db.none('INSERT INTO users(username,password) VALUES($1,$2)',[username,hash])
-                    .then(()=>{
-                    res.send("SUCCESS");
-                    })
-                }
-            })
-            
-        }
-    })
-})
-app.get('/register',(req,res)=>{
-    res.render('register');
-})
-
-app.get('/users/add-article',(req,res)=>{
-    res.render('add-article');
-})
-
-app.post('/users/add-article',(req,res)=>{
-    let title=req.body.title;
-    let description=req.body.description;
-    let userId= req.session.user.userId;
-    db.none('INSERT INTO articles(title,body,userid) VALUES($1,$2,$3)',[title,description,userId])
-    .then(()=>{
-        res.send("success!")
-    })
-})
-app.listen(PORT,()=>{
-    console.log("server started at "+PORT);
-})
+app.listen(PORT,() => {
+  console.log(`Server has started on ${PORT}`)
+});
